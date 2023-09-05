71bc6ff9f34e079ff8df12b006fc3ac27a03e4d9
ferradjielie <ferradji.elie@outlook.fr>
2023-09-05 15:57:14 +0200

    ajout de css via bootstrap

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
     <title>Ajout produit</title>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-link" href="index.php">index</a>
+    <a class="navbar-link" href="recap.php">recap</a>
+   
+  </div>
+</nav>
     <h1>Ajouter un produit </h1>
     <form action="traitement.php" method="POST"> 
         <p>
@@ -36,6 +46,9 @@
      
     
     </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
     
 </body>
 </html>
